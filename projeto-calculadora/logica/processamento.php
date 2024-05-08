234bb62e5b0629961e50ca91adbc1e1f07ee7e47
Arthur <nonakaodaarthur@gmail.com>
2024-05-08 14:24:56 -0300

    commit

---
+++ projeto-calculadora/logica/processamento.php
@@ -0,0 +1,27 @@
+<?php
+require_once "funcoesCalculo.php";
+
+$result;
+
+if(isset($_POST['inputNum1']) && isset($_POST['inputNum2'])){
+    $num1 = $_POST['inputNum1'];
+    $num2 = $_POST['inputNum2'];
+    switch($_POST['selectOperacoes']){
+        case "adicao":
+            $result = $num1 ." + ". $num2 . " = " . soma($num1,$num2);
+            break;
+        case "subtracao":
+            $result = $num1 ." - ". $num2 . " = " . subtracao($num1,$num2);
+            break;
+        case "multiplicacao":
+            $result = $num1 ." * ". $num2 . " = " . multiplicacao($num1,$num2);
+            break;
+        case "divisao":
+            $result = $num1 ." / ". $num2 . " = " . divisao($num1,$num2);
+            break;
+
+    }
+
+}
+
+Echo $result;
